5051c64be5fc39190cdc72ef118edd9b8d8faf74
pablo.raspo <pablo.raspo@EIVNOTEBOOK_01.mupim.local>
2019-03-18 16:29:15 -0300

    Factorial por alter. 1

--- calculo-factorial/src/main/java/com/eiv/factorial/App.java
+++ calculo-factorial/src/main/java/com/eiv/factorial/App.java
@@ -1,8 +1,20 @@
 package com.eiv.factorial;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Factorial");
+ 
+        System.out.println("Ingrese un numero:");
+        Scanner s = new Scanner(System.in);
+        String sNumero = s.nextLine();
+        int numero = Integer.valueOf(sNumero);
+        
+        CalculoFactorial factorial = new CalculoFactorialImpl();
+        long resultado = factorial.calcular(numero);
+        System.out.println("Factorial de " + numero + " es " + resultado);
+        
+        s.close();
     }
 }
